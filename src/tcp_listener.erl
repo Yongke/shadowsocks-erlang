9ba53f523deb1e2d154f176e3d9b8bf96751062d
Wang Yongke <wangyongke@gmail.com>
2013-05-21 11:57:10 +0800

    Remove comment in tcp_listener.erl

--- src/tcp_listener.erl
+++ src/tcp_listener.erl
@@ -2,7 +2,7 @@
 %%% @author Yongke <wangyongke@gmail.com>
 %%% @copyright (C) 2013, Yongke
 %%% @doc
-%%%
+%%% Tcp listener
 %%% @end
 %%%-------------------------------------------------------------------
 -module(tcp_listener).
@@ -31,14 +31,6 @@
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the listening process
-%%
-%% @spec start_link(Port::integer(), Module::atom()) -> 
-%%                             {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link(Port, Module, Args) when is_integer(Port), is_atom(Module)->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [Port, Module, Args], []).
 
@@ -46,16 +38,6 @@ start_link(Port, Module, Args) when is_integer(Port), is_atom(Module)->
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Create listening socket
-%%
-%% @spec init(Port::integer(), Module::atom()) -> 
-%%                     {ok, State} |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([Port, Module, Args]) ->
     process_flag(trap_exit, true),
     Opts = [binary, {packet, 2}, {reuseaddr, true},
@@ -71,42 +53,13 @@ init([Port, Module, Args]) ->
             {stop, Reason}
     end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This gen_server only handle tcp data, so any gen_server call will 
-%% stop the server
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
+
 handle_call(Request, _From, State) ->
     {stop, {unknown_call, Request}, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_info({inet_async, ListSock, Ref, {ok, CliSocket}},
             #state{listener=ListSock, acceptor=Ref, module=Module,
                    module_args = Args} 
@@ -143,29 +96,10 @@ handle_info({inet_async, ListSock, Ref, Error},
 handle_info(_Info, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 terminate(_Reason, State) ->
     gen_tcp:close(State#state.listener),
     ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
